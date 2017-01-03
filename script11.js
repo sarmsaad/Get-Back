c5d0e3b7cca611be57a8a5f7f33858cb222aca67
sarmsaad <sarah.msaad@yahoo.com>
2017-01-03 13:04:16 +0100

    initial work

---
+++ script11.js
@@ -0,0 +1,57 @@
+function initMap() {
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var directionsService = new google.maps.DirectionsService;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: {lat: 41.878114, lng: -87.629798}
+  });
+
+ // directionsDisplay.setMap(map);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      var infowindow = new google.maps.InfoWindow();
+      var posl = {
+        lat: 41.878114,
+        lng: -87.629798
+      };
+
+      //map.setCenter(posl);
+      directionsDisplay.setMap(map);
+      directionsDisplay.setPanel(document.getElementById('right-panel'));
+      calculateAndDisplayRoute(directionsService, directionsDisplay,posl);
+      document.getElementById('mode').addEventListener('change', function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay,posl);
+      });
+
+
+
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay,pos) {
+  var selectedMode = document.getElementById('mode').value;
+  directionsService.route({
+    origin: pos,  
+    destination: {lat: 37.785636, lng: -122.397119}, 
+    travelMode: google.maps.TravelMode[selectedMode]
+  }, function(response, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+
